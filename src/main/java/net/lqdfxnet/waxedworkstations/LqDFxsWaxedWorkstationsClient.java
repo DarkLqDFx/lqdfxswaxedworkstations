362d4f34b38b79630be421eaa11103960015a150
Russell Lemon <lqdfx@hotmail.com>
2026-02-12 12:46:23 -0500

    Commented out code Not being used. Removed unused imports

--- src/main/java/net/lqdfxnet/waxedworkstations/LqDFxsWaxedWorkstationsClient.java
+++ src/main/java/net/lqdfxnet/waxedworkstations/LqDFxsWaxedWorkstationsClient.java
@@ -1,6 +1,5 @@
 package net.lqdfxnet.waxedworkstations;
 
-import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
